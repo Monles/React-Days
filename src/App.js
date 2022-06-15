746fa133006f96f4d9c01fa23c7a6602c1240cfb
monles <iceueb@gmail.com>
2022-06-15 10:36:16 +0100

    day05

--- src/App.js
+++ src/App.js
@@ -11,14 +11,24 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
-      console.log(response)
-    );
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
+        <input class />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
